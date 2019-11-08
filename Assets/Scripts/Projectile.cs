ffefabc6a46f9f270563ba426cbfcdcf4bd69a4a
Liutauras <liutauras.gaidamavicius@gmail.com>
2019-11-08 20:35:47 +0200

    projectile logic

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    [SerializeField] private float destroyTimer = 5.0f;
+    [SerializeField] private float projectileSpeed = 20;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = Vector3.up * projectileSpeed;
+        Destroy(this.gameObject, destroyTimer);
+    }
+
+}
---
+++ Assets/Scripts/ProjectileLauncher.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ProjectileLauncher : MonoBehaviour
+{
+    [SerializeField] private GameObject projectilePrefab = default;
+
+    [SerializeField] private Vector3 launchOffset;
+
+    private IShipInput input;
+
+    private void Awake() {
+        input = GetComponent<IShipInput>();
+    }
+
+    private void OnEnable() {
+        input.OnPrimaryFire += ShootProjectile;
+    }
+    
+    private void OnDisable(){
+        input.OnPrimaryFire -= ShootProjectile;
+    }
+
+    private void ShootProjectile(){
+        Instantiate(projectilePrefab, transform.position + launchOffset, Quaternion.identity);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position + launchOffset, 0.05f);
+    }
+}
