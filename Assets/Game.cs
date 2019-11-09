4cd185c95b962a8be141bf7b0bdc208fdc34b4c8
Liutauras <liutauras.gaidamavicius@gmail.com>
2019-11-09 20:21:25 +0200

    added space invaders styled left-to-right movement

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -5,18 +5,90 @@ using UnityEngine;
 public class Game : MonoBehaviour
 {
     [SerializeField] private ShipFactory enemyFactory;
+    [SerializeField] private float moveDelay;
+    [SerializeField] private float lineMoveTimerOffset = 0.15f;
+
+    private float moveTimer = 0.0f;
+    private float lineOffsetTimer = 0.0f;
+
+    private int currentLineIndex = 0;
+
+    private bool currentLineMoved = false;
+
+    private float speedX = 0.3f;
+    public bool moveLeft = true; 
+    private bool newMoveLeft = true;
+
+    private List<List<GameObject>> enemyShips;
 
     void Start(){
+        enemyShips = new List<List<GameObject>>();
         GenerateEnemies();
     }
 
     void GenerateEnemies(){
-        for(int i = -6; i <= 6; i+=2){
-            for(int j = 0; j <= 3; ++j){
+        for(int i = 0; i <= 3; i++){
+            List<GameObject> shipLine = new List<GameObject>();
+            for(int j = -6; j <= 6; j+=2){
                 var ship = enemyFactory.Get();
-                ship.transform.position = new Vector3(i, j, 0);
+                ship.transform.position = new Vector3(j, i, 0);
+                shipLine.Add(ship.gameObject);
             }
+            enemyShips.Add(shipLine);
         }
     }
+
+    void Update(){
+        float deltaX = moveLeft ? -speedX : speedX;
+        //Move ship group
+        if(moveTimer >= moveDelay){
+            if(lineOffsetTimer >= lineMoveTimerOffset){
+                currentLineMoved = false;
+                currentLineIndex++;
+                if(currentLineIndex >= enemyShips.Count){
+                    Debug.Log("last line moved");
+                    currentLineIndex = 0;
+                    moveTimer = 0.0f;
+                    moveLeft = newMoveLeft;
+                    deltaX = moveLeft ? -speedX : speedX;
+                }
+                lineOffsetTimer = 0.0f;
+            }else{
+                lineOffsetTimer += Time.deltaTime;
+            }
+            if(!currentLineMoved){
+                for(int i = enemyShips[currentLineIndex].Count - 1; i >= 0; i--){
+                    if(enemyShips[currentLineIndex][i]){
+                        enemyShips[currentLineIndex][i].transform.Translate(new Vector3(deltaX, 0, 0), Space.World);
+                    }else{
+                        enemyShips[currentLineIndex].RemoveAt(i);
+                    }
+                }
+                currentLineMoved = true;
+            }
+        }else{
+            moveTimer += Time.deltaTime;
+        }
+
+        //Check if deltaX should change
+        bool exitLoop = false;
+        foreach(var line in enemyShips){
+            foreach(var ship in line){
+                if(!ship) continue;
+                if(ship.transform.position.x <= -8.0f){
+                    newMoveLeft = false;
+                    exitLoop = true;
+                    break;
+                }
+                if(ship.transform.position.x >= 8.0f){
+                    newMoveLeft = true;
+                    exitLoop = true;
+                    break;
+                }
+            }
+            if(exitLoop) break;
+        }
+    }
+
 
 }
--- Assets/Scripts/EnemyInput.cs
+++ Assets/Scripts/EnemyInput.cs
@@ -9,21 +9,4 @@ public class EnemyInput : MonoBehaviour, IShipInput
     public event Action OnPrimaryFire = delegate{};
     public event Action OnSecondaryFire = delegate{};
 
-    private void Start()
-    {
-        Horizontal = 1.0f;
-        Vertical = 0.0f;
-    }
-
-    private void Update()
-    {
-        if(transform.position.x >= 7.0f || transform.position.x <= -7.0f){
-            Horizontal = -Horizontal;
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        Horizontal = -Horizontal;
-    }
 }
